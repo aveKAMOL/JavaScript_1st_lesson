552202474ca04e8db438dd79829728259841046c
Kamol Qurbonov <kamol2008q@gmail.com>
2023-06-02 13:27:48 +0500

    .

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let name = "Mening ismim Kamol"
+let last_name = "Mening familiyam Qurbonov"
+let age = "Mening 15daman"
+let education = "Men maktabda o'qiyman"
+let hobbi = "Mening hobbiyim dasturlesh"
+
+let str = name + "; " + last_name + "; " + age + "; " + education + "; " + hobbi + "."
+
+console.log(str)
